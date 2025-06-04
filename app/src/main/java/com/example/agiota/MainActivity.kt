1f0a482a01f0418443a3b26a2e2f6960b6ad20d5
João Pedro Silva Antunes <antunesjoaopedro3@gmail.com>
2025-06-04 20:18:53 -0300

    ajustes das paginas + interatividade

--- app/src/main/java/com/example/agiota/MainActivity.kt
+++ app/src/main/java/com/example/agiota/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.agiota
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.home)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Aqui você pega o botão e configura o clique para abrir a DepositoActivity
+        val buttonDeposito = findViewById<MaterialButton>(R.id.buttonDeposito)
+        buttonDeposito.setOnClickListener {
+            val intent = Intent(this, DepositoActivity::class.java)
+            startActivity(intent)
         }
     }
 }
